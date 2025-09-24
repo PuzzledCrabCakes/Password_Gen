5f0b909760da798b26dfcc1bfa94700a7d46b17b
PuzzledCrabCakes <clark.adamm.m@gmail.com>
2025-09-24 18:20:01 -0400

    Added TODO for test() and fixed small logic

--- pass_gen_v2.py
+++ pass_gen_v2.py
@@ -1,26 +1,32 @@
 import random
 
+
 def pass_gen():
-    char = 'a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 ! @'
-    usable_char = char.split(' ')
+    char = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 ! @"
+    usable_char = char.split(" ")
     password_part = random.sample(usable_char, 4)
-    final = ''.join(password_part)
+    final = "".join(password_part)
     return final
 
-password = f'{pass_gen()}-{pass_gen()}-{pass_gen()}'
+
+password = f"{pass_gen()}-{pass_gen()}-{pass_gen()}"
 print(password)
 
+
+# TODO:
+# Fix this test password. the logic is not there and isn't working like I wanted. Will address later.
 def test_password():
     points = 0
     for i in password:
         if i.isalnum():
             points += 1
-        elif i.isalpha() == False:
+        elif not i.isalpha():
             points += 1
         else:
             points -= 1
 
-    return 'Strong' if points == 14 else 'Weak'
+    return "Strong" if points == 14 else "Weak"
+
 
 test = test_password()
 print(test)
